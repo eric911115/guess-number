b6c3fec958a4c62d762d492b2d2b34fc0295ffca
eric wei <aeric911115@gmail.com>
2020-05-23 08:58:51 +0800

    add times

--- number.py
+++ number.py
@@ -1,6 +1,8 @@
 import random 
 r = random.randint(1, 100)
+x = 0
 while True:
+	x = x + 1
 	number = input('請輸入數字')
 	number = int(number)
 	if number == r:
@@ -10,3 +12,4 @@ while True:
 		print('比答案大')
 	elif number < r:
 		print('比答案小')
+	print('你已經猜第', x, '次了')
