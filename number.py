bb8c0658e98c7ace538ed44f892fccfc73bc600f
eric wei <aeric911115@gmail.com>
2020-05-23 08:57:20 +0800

    guess number

---
+++ number.py
@@ -0,0 +1,12 @@
+import random 
+r = random.randint(1, 100)
+while True:
+	number = input('請輸入數字')
+	number = int(number)
+	if number == r:
+		print('終於猜對了!')
+		break
+	elif number > r:
+		print('比答案大')
+	elif number < r:
+		print('比答案小')
