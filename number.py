19a27884691771cd4e78d50c252a4166ad2f765a
eric wei <aeric911115@gmail.com>
2020-05-23 09:08:46 +0800

    add user decide number

--- number.py
+++ number.py
@@ -1,12 +1,17 @@
 import random 
-r = random.randint(1, 100)
+start = input('請使用者決定開始值')
+end = input('請使用者決定結束值')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
 x = 0
 while True:
-	x = x + 1
+	x += 1
 	number = input('請輸入數字')
 	number = int(number)
 	if number == r:
 		print('終於猜對了!')
+		print('你已經猜第', x, '次了')
 		break
 	elif number > r:
 		print('比答案大')
